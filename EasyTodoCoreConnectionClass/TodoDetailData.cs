f4084248267b3ae9bbb8104c7270cdb3927a02d6
rai <rai002@yahoo.co.jp>
2017-09-05 23:33:19 +0900

    クラス追加

---
+++ EasyTodoCoreConnectionClass/TodoDetailData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EasyTodoCoreConnectionClass
+{
+    public class TodoDetailData
+	{
+		public int UserId { get; set; }
+		public int DataId { get; set; }
+		public String Title { get; set; }
+		public String Detail { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public int State { get; set; }
+		public int ColorId { get; set; }
+	}
+}
