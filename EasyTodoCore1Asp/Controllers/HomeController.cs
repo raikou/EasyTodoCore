d98436033169f404fc122b73e458f7b73d516109
rai <rai002@yahoo.co.jp>
2017-09-10 07:29:15 +0900

    Core1.1も試してみる

--- EasyTodoCore1Asp/Controllers/HomeController.cs
+++ EasyTodoCore1Asp/Controllers/HomeController.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using EasyTodoCore2Asp.Models;
 
-namespace EasyTodoCore2Asp.Controllers
+namespace EasyTodoCore1Asp.Controllers
 {
     public class HomeController : Controller
     {
-	    CoreContext.todo_dataModel context = new CoreContext.todo_dataModel();
-
-		public IActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult About()
         {
-	        ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
@@ -33,7 +29,7 @@ namespace EasyTodoCore2Asp.Controllers
 
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
---
+++ EasyTodoCore2Asp/Controllers/TodoDetailDatasController.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoreContext;
+
+namespace EasyTodoCore2Asp.Controllers
+{
+	[Produces("application/json")]
+    [Route("api/TodoDetailDatas")]
+    public class TodoDetailDatasController : Controller
+    {
+	    private readonly CoreContext.todo_dataModel _context;
+
+        public TodoDetailDatasController()
+        {
+            _context = new CoreContext.todo_dataModel();
+		}
+
+		// GET: api/TodoDetailDatas
+		[HttpGet]
+        public IEnumerable<TodoDetailData> GetTodoDetailDatas()
+		{
+			var data = _context.TodoDetailDatas.Where(x => x.UserId == 0);
+
+			return _context.TodoDetailDatas;
+        }
+
+        // GET: api/TodoDetailDatas/5
+        [HttpGet("{UserId}/{DataId}")]
+        public async Task<IActionResult> GetTodoDetailData([FromRoute] int UserId, [FromRoute] int DataId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoDetailData = await _context.TodoDetailDatas.SingleOrDefaultAsync(m => m.UserId == UserId && m.DataId == DataId);
+
+            if (todoDetailData == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todoDetailData);
+        }
+
+        // PUT: api/TodoDetailDatas/5
+        [HttpPut("{UserId}/{DataId}")]
+        public async Task<IActionResult> PutTodoDetailData([FromRoute] int UserId, [FromRoute] int DataId, [FromBody] TodoDetailData todoDetailData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (UserId != todoDetailData.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoDetailData).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoDetailDataExists(UserId, DataId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoDetailDatas
+        [HttpPost]
+        public async Task<IActionResult> PostTodoDetailData([FromBody] TodoDetailData todoDetailData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.TodoDetailDatas.Add(todoDetailData);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TodoDetailDataExists(todoDetailData.UserId, todoDetailData.DataId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTodoDetailData", new { UserId = todoDetailData.UserId, DataId = todoDetailData.DataId }, todoDetailData);
+        }
+
+        // DELETE: api/TodoDetailDatas/5
+        [HttpDelete("{UserId}")]
+        public async Task<IActionResult> DeleteTodoDetailData([FromRoute] int UserId, [FromRoute] int DataId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoDetailData = await _context.TodoDetailDatas.SingleOrDefaultAsync(m => m.UserId == UserId && m.DataId == DataId);
+            if (todoDetailData == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoDetailDatas.Remove(todoDetailData);
+            await _context.SaveChangesAsync();
+
+            return Ok(todoDetailData);
+        }
+
+        private bool TodoDetailDataExists(int UserId, int DataId)
+        {
+            return _context.TodoDetailDatas.Any(e => e.UserId == UserId && e.DataId == DataId);
+        }
+    }
+}
