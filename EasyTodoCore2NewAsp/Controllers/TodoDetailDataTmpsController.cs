5c92940516271c0a31ac79977b87005e627be468
rai <rai002@yahoo.co.jp>
2017-09-10 16:05:54 +0900

    コントローラー追加

---
+++ EasyTodoCore2NewAsp/Controllers/TodoDetailDataTmpsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EasyTodoCore2NewAsp.Models;
+using EasyTodoCoreConnectionClass;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTodoCore2NewAsp.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/TodoDetailDataTmps")]
+	public class TodoDetailDataTmpsController : Controller
+	{
+		public TodoDetailDataTmpsController()
+		{
+		}
+
+		// GET: api/TodoDetailDatas
+		[HttpGet]
+		public IEnumerable<TodoDetailData> GetTodoDetailDatas()
+		{
+			using (var context = new PgContext())
+			{
+				var dataList = context.TodoDetailDataTmpPgs.ToList();
+				var result = Clone.Convert<TodoDetailData, TodoDetailDataTmpPg >(dataList);
+				return result;
+			}
+		}
+	}
+}
